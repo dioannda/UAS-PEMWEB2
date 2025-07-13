a74c2503f5b42f072f69f1beee79c6c7058e892d
rizkiset <mrizkisetiobudi@saintek.unipdu.ac.id>
2025-07-13 16:10:00 +0700

    nambah navbar

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -10,23 +10,14 @@
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/dashboard">KeuanganKu</a>
+            <a class="navbar-brand" href="/dashboard">Finote</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="/transaksi">Transaksi</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/pengingat">Pengingat</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="/laporan">Laporan</a>
-                    </li>
+                <ul class="navbar-nav ms-auto">     
                     <li class="nav-item">
                         <a class="nav-link text-danger" href="/logout">Logout</a>
                     </li>
